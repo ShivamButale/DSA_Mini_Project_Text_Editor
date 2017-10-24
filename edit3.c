8ecfe2de290d038e929461af04a3778505720474
Shivam Butale <shivbutale22@gmail.com>
2017-10-24 13:08:15 +0000

    Upload New File

---
+++ edit3.c
@@ -0,0 +1,227 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
+#define CTRL_KEY(k) ((k) & 0x1f)
+enum editorKey {
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+  	ARROW_UP,
+  	ARROW_DOWN,
+  	DEL_KEY,
+  	HOME_KEY,
+  	END_KEY,
+  	PAGE_UP,
+  	PAGE_DOWN
+};
+struct editorConfig {
+  	int cx, cy;//controls the x and y coordinates i.e positions of the cursor
+  	int screenrows;//number of rows on the screen
+  	int screencols;//number of columns on the screen
+  	struct termios initial;
+};
+struct editorConfig E;
+void die(const char *s) {//printing the error message in case of failure
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+  	perror(s);
+  	exit(1);
+}
+void disableRawMode() {//Leave raw mode
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.initial) == -1)
+    	die("tcsetattr");
+}
+void enableRawMode() {
+	if (tcgetattr(STDIN_FILENO, &E.initial) == -1) die("tcgetattr");
+  	atexit(disableRawMode);
+  	struct termios raw = E.initial;
+  	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);//conto; various signals by combinations of keys
+  	raw.c_oflag &= ~(OPOST);//stop output processing
+  	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);//stop printing, read line by line, stop ctrl-c, ctrl-z
+  	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+int editorReadKey() {//interpreting the various keypresses 
+	int nread;
+ 	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    		if (nread == -1 && errno != EAGAIN) die("read");
+  	}
+	if (c == '\x1b') {
+    		char seq[3];
+		if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    		if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+		if (seq[0] == '[') {
+        		if (seq[1] >= '0' && seq[1] <= '9') {
+        			if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        			if (seq[2] == '~') {
+        				switch (seq[1]) {
+        					case '1': return HOME_KEY;
+        					case '3': return DEL_KEY;
+        		        		case '4': return END_KEY;
+        		        		case '5': return PAGE_UP;
+        		        		case '6': return PAGE_DOWN;
+        		        		case '7': return HOME_KEY;
+        		   			case '8': return END_KEY;
+        				}
+        			}
+      			} 
+      			else {
+        			switch (seq[1]) {
+          				case 'A': return ARROW_UP;
+          				case 'B': return ARROW_DOWN;
+    		      			case 'C': return ARROW_RIGHT;
+          				case 'D': return ARROW_LEFT;
+          				case 'H': return HOME_KEY;
+          				case 'F': return END_KEY;
+        			}
+        		}
+   	 	} 
+   	 	else if (seq[0] == 'O') {
+      			switch (seq[1]) {
+        			case 'H': return HOME_KEY;
+       	 			case 'F': return END_KEY;
+      			}
+    		}
+	return '\x1b';
+  	} 
+  	else {
+    		return c;
+  	}
+}
+int getCursorPosition(int *rows, int *cols) {
+	char buf[32];
+	unsigned int i = 0;
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+	while (i < sizeof(buf) - 1) {
+    		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    		if (buf[i] == 'R') break;
+    		i++;
+  	}
+  	buf[i] = '\0';
+	if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+	return 0;
+}
+int getWindowSize(int *rows, int *cols) {//get size of terminal window to print proper number of tildes
+	struct winsize ws;
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    		return getCursorPosition(rows, cols);
+  	} 
+  	else {
+    		*cols = ws.ws_col;
+    		*rows = ws.ws_row;
+    		return 0;
+  	}
+}
+struct abuf {//new structure so that no need to call write again and again
+	char *b;
+  	int len;
+};
+#define ABUF_INIT {NULL, 0}
+void abAppend(struct abuf *ab, const char *s, int len) {
+	char *new = realloc(ab->b, ab->len + len);
+	if (new == NULL) return;
+  	memcpy(&new[ab->len], s, len);
+  	ab->b = new;
+  	ab->len += len;
+}
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
+void editorDrawRows(struct abuf *ab) {//printing the tildes to gain vi like interface
+	int y;
+	for (y = 0; y < E.screenrows; y++) {
+      		abAppend(ab, "~", 1);
+		abAppend(ab, "\x1b[K", 3);
+    		if (y < E.screenrows - 1) {
+      			abAppend(ab, "\r\n", 2);
+    		}
+  	}
+}
+void editorRefreshScreen() {//set cursor and print tildes
+	struct abuf ab = ABUF_INIT;
+	abAppend(&ab, "\x1b[?25l", 6);
+	abAppend(&ab, "\x1b[H", 3);
+	editorDrawRows(&ab);
+	char buf[32];
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  	abAppend(&ab, buf, strlen(buf));
+	abAppend(&ab, "\x1b[?25h", 6);
+	write(STDOUT_FILENO, ab.b, ab.len);
+  	abFree(&ab);
+}
+void editorMoveCursor(int key) {//controlling the cursor position based on the keypresses
+	switch (key) {
+    		case ARROW_LEFT:
+      			if (E.cx != 0) {
+        			E.cx--;
+      			}
+      			break;
+   	 	case ARROW_RIGHT:
+      			if (E.cx != E.screencols - 1) {
+      				  E.cx++;
+      			}
+      			break;
+    		case ARROW_UP:
+      			if (E.cy != 0) {
+        			E.cy--;
+      			}	
+      			break;
+    		case ARROW_DOWN:
+      			if (E.cy != E.screenrows - 1) {
+        			E.cy++;
+      			}		
+      			break;
+  	}
+}
+void editorProcessKeypress() {//handling keypresses. It handles ctrl-q for quit, homekey
+	int c = editorReadKey();
+	switch (c) {
+		case CTRL_KEY('q'):
+	      		write(STDOUT_FILENO, "\x1b[2J", 4);
+	      		write(STDOUT_FILENO, "\x1b[H", 3);
+	      		exit(0);
+	      		break;
+		case HOME_KEY:
+ 			E.cx = 0;
+      			break;
+		case END_KEY:
+      			E.cx = E.screencols - 1;
+      			break;
+		case PAGE_UP:
+	        case PAGE_DOWN:
+      			{
+        		int times = E.screenrows;
+        		while (times--)
+          		editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      			}
+      			break;
+
+    		case ARROW_UP:
+    		case ARROW_DOWN:
+    		case ARROW_LEFT:
+    		case ARROW_RIGHT:
+      			editorMoveCursor(c);
+      			break;
+  	}
+}
+void initEditor() {//initializes editor configurations
+	E.cx = 0;
+	E.cy = 0;
+	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+int main() {
+	enableRawMode();//we dont see keypresses currently
+	initEditor();
+	while (1) {
+    		editorRefreshScreen();
+    		editorProcessKeypress();
+  	}
+	return 0;
+}
+
